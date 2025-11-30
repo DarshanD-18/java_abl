3d04f573ee19b2da557736cd9c13f6d0dac303e2
DarshanD-18 <darshandghl@gmail.com>
2025-11-30 12:54:31 +0530

    core-project->darshan

---
+++ src/main/java/net/javaguides/banking/Exception/AccountException.java
@@ -0,0 +1,10 @@
+package net.javaguides.banking.Exception;
+
+public class AccountException extends RuntimeException{
+
+    public AccountException(String message){
+        super(message);
+
+    }
+}
+
